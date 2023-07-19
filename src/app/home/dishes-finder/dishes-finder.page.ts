5c4ad6797fbea0cd8ba69a924965bbe2c6c1334c
RaffyRod <elraffy3@gmail.com>
2023-07-19 00:30:59 -0400

    filters added

--- src/app/home/dishes-finder/dishes-finder.page.ts
+++ src/app/home/dishes-finder/dishes-finder.page.ts
@@ -16,6 +16,10 @@ export class DishesFinderPage implements OnInit {
   public findBar: any;
   public dishes: any;
   public plato:any;
+  public selector:any = 'nombre';
+  public caloriasBar: any = 0;
+  public maxCalorias:number = 100;
+  public minCalorias:number = 0;
 
   @ViewChild(IonModal) modal: IonModal | any;
 
@@ -36,13 +40,6 @@ export class DishesFinderPage implements OnInit {
   abrirModal(receta:any){
     this.setOpen(true);
     this.plato = receta;
-    this.plato.Entrada.ingredientesCantidad = [];
-    this.plato.Entrada.preparacion = this.plato.Entrada.Preparación;
-    receta.Entrada.Ingredientes.forEach((ingrediente:string,key:number) => {
-      let concatenacion = receta.Entrada.Cantidad[key]+' de '+ingrediente;
-      this.plato.Entrada.ingredientesCantidad.push(concatenacion);
-    });
-    console.log(this.plato)
   }
   onWillDismiss(event: Event) {
     this.setOpen(false);
@@ -52,21 +49,58 @@ export class DishesFinderPage implements OnInit {
 
   ngOnInit() {
     this.getRecetas();
+
   }
 
   filtrarRecetas(){
     this.platos = this.dishes;
-    let filtrados = [];
-    filtrados = this.platos.filter((plato: { Nombre_receta: string })=>plato.Nombre_receta.toLowerCase().includes(this.findBar.toLowerCase()))
+    let filtrados:any = [];
+    switch(this.selector){
+      case 'nombre':
+        this.caloriasBar = this.maxCalorias;
+        this.dishes.forEach((dish:any) => {
+          if(dish.Receta.Nombre_receta.toLowerCase().includes(this.findBar.toLowerCase())){
+            filtrados.push(dish);
+          }
+        });
+        this.platos = filtrados;
+        break;
+      case 'caloria':
+        this.platos = [];
+        this.findBar = "";
+        this.dishes.forEach((plato:any) => {
+          if(plato.Energia_Kcal <= this.caloriasBar){
+            filtrados.push(plato);
+          }
+        });
+        this.platos = filtrados;
+        break;
+    }
+
+
     this.platos = filtrados;
   }
 
 
 
+  getTopesCalorias(){
+    let calorias:any = [];
+    this.platos.forEach((plato:any) => {
+      if(!calorias.includes(plato.Energia_Kcal)){
+        calorias.push(plato.Energia_Kcal);
+      }
+    });
+    calorias.sort();
+    this.minCalorias = calorias[0];
+    this.maxCalorias = calorias[calorias.length-1]+10;
+    this.caloriasBar = this.maxCalorias;
+  }
+
   getRecetas(){
     this.recetasDB.getRecetas().then(data => {
       this.platos = data;
       this.dishes = data;
+      this.getTopesCalorias();
     })
 
   }
